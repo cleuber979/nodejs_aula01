7735f43f149bce58446630698c06549bfc935b7f
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2023-12-09 21:55:43 -0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,13 +1,1 @@
- //module.exports = function(x,y){
-    //return x * y;
-// };
 
- module.exports = class cachorro{
-    constructor(nome){
-        this.nome = nome;
-    }
-    latir(){
-      console.log(`${this.nome}  esta latindo au au`);
-    }
-        
-    };
