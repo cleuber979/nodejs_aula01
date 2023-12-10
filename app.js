087e937bd18abf58f03c998b5de52e272d04cedc
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2023-12-09 21:55:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-//const multiplicacao = require( './mod');
 
-//console.log(multiplicacao(2,2));
-
-const Cahorro = require('./mod');
-const cachorrinho = new Cahorro ( 'dog' );
-cachorrinho.latir();
